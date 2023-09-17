e5c3e2b60edc03b058b1fb8d15a8ee005853d1b5
Maksim Atenyaev <atenjaev@gmail.com>
2023-09-18 03:42:56 +0600

    homework

--- src/test/java/DragAndDrop.java
+++ src/test/java/DragAndDrop.java
@@ -8,7 +8,6 @@ import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-
 public class DragAndDrop {
 
     String uri = "/drag_and_drop";
